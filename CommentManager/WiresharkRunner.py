d48a3da526c29a838abc8b7549bae252dec2baa4
raistlinJ <jaime.acosta1@users.noreply.github.com>
2020-08-31 13:51:15 -0600

    Wireshark crash no longer crashes entire system

--- CommentManager/WiresharkRunner.py
+++ CommentManager/WiresharkRunner.py
@@ -1,7 +1,7 @@
 import logging
 import subprocess
 import shlex
-import sys
+import sys, traceback
 from ConfigurationManager.ConfigurationManager import ConfigurationManager
 from PyQt5.QtCore import QThread
 
@@ -10,21 +10,30 @@ class WiresharkRunner(QThread):
     def __init__(self, lua_scripts=None, pcap_filename=None):
         logging.debug('WiresharkRunner(): Instantiated')
         QThread.__init__(self)
-        self.cmd = ConfigurationManager.get_instance().read_config_abspath("COMMENT_MANAGER","WIRESHARK_FILENAME")
-        if pcap_filename != None:
-            self.cmd+= " -r " + pcap_filename
-        if lua_scripts != None and len(lua_scripts) > 0:
-            for lua_script in lua_scripts:
-                self.cmd+= " -Xlua_script:" + lua_script
-        logging.debug('WiresharkRunner(): Complete')
-
+        try:
+                
+            self.cmd = ConfigurationManager.get_instance().read_config_abspath("COMMENT_MANAGER","WIRESHARK_FILENAME")
+            if pcap_filename != None:
+                self.cmd+= " -r " + pcap_filename
+            if lua_scripts != None and len(lua_scripts) > 0:
+                for lua_script in lua_scripts:
+                    self.cmd+= " -Xlua_script:" + lua_script
+            logging.debug('WiresharkRunner(): Complete')
+        except Exception as e:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            logging.error('WiresharkRunner(): Error during Wireshark execution')
+            traceback.print_exception(exc_type, exc_value, exc_traceback)
     def run(self):
         logging.debug('WiresharkRunner.run(): Instantiated')
-        if sys.platform == "linux" or sys.platform == "linux2":
-            logging.debug('WiresharkRunner.run(): Running command: ' + str(self.cmd))
-            output = subprocess.check_output(shlex.split(self.cmd))
-        else: 
-            output = subprocess.check_output(self.cmd)
+        try:
+            if sys.platform == "linux" or sys.platform == "linux2":
+                logging.debug('WiresharkRunner.run(): Running command: ' + str(self.cmd))
+                output = subprocess.check_output(shlex.split(self.cmd))
+            else: 
+                output = subprocess.check_output(self.cmd)
 
-        logging.debug('WiresharkRunner.run(): Complete')
-
+            logging.debug('WiresharkRunner.run(): Complete')
+        except Exception as e:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            logging.error('WiresharkRunner(): Error during Wireshark execution')
+            traceback.print_exception(exc_type, exc_value, exc_traceback)
